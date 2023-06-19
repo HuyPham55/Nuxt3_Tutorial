f19ca47d4498734421a8f1200b048493a0385cdd
huydu <huyducphamofficial@gmail.com>
2023-06-19 10:47:35 +0700

    Feat: Added error pages
    Details: https://youtu.be/L9suY4qAFrQ

---
+++ error.vue
@@ -0,0 +1,24 @@
+<script setup>
+defineProps(['error'])
+const handleClearError = () => {
+  clearError({redirect: '/'})
+}
+</script>
+
+<template>
+  <div class="mt-7 max-w-sm mx-auto text-center card">
+    <h1 class="text-7xl my-7 font-bold">
+      {{ error.statusCode }}
+    </h1>
+    <p class="text-xl my-7">
+      {{ error.message }}
+    </p>
+    <button class="btn my-7" @click="handleClearError">
+      Back to home
+    </button>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -1,4 +1,4 @@
-<script setup lang="js">
+<script setup lang="ts">
 const {id} = useRoute().params
 definePageMeta({
   layout: 'products',
@@ -8,6 +8,9 @@ const uri = "https://fakestoreapi.com/products/" + id
 //Telling Nuxt that each fetch is different
 const {data: product} = await useFetch(uri, { key: id })
 
+if (!product.value) {
+  throw createError({statusCode: 404, statusMessage: 'Product not found', fatal: true})
+}
 </script>
 
 <template>
